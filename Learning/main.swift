86f388e762ad4cf34a234d6dc4ff17770bea755a
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-04-23 14:53:33 +0200

    Initial commit

---
+++ Learning/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Learning
+//
+//  Created by Artem Zhukov on 23.04.20.
+//  Copyright © 2020 Artem Zhukov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
