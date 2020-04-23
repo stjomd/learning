41c460e6cd99ea633892aaa22ed0c88a5199a228
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-04-23 20:07:39 +0200

    Add documentation

--- Learning/Algorithms/Sorting/Sorting.swift
+++ Learning/Algorithms/Sorting/Sorting.swift
@@ -8,11 +8,29 @@
 
 // MARK: - Main Extension
 enum SortingAlgorithm {
-    // Add a new case for every new sorting algorithm.
+    /// - Complexity: O(*n*^2)
     case bubbleSort
 }
 extension Array where Element: Comparable {
     typealias Comparator = (Element, Element) -> Bool
+    /// Sorts the array in-place using the specified algorithm.
+    ///
+    /// You do not have to specify the last parameter. By default, the array will be sorted in ascending order (as defined for the respective
+    /// type).
+    ///
+    ///     var array = [3, 1, 8, 2]
+    ///     array.customSort(.bubbleSort)
+    ///     // array = [1, 2, 3, 8]
+    ///     array.customSort(.bubbleSort, >)
+    ///     // array = [8, 3, 2, 1]
+    ///
+    /// If you wish to sort in descending order, pass `>` as the second parameter.
+    ///
+    /// For available algorithms, see `SortingAlgorithm`.
+    /// - Complexity: See the documentation for the passed `algorithm` variable.
+    /// - Parameter algorithm: The algorithm to be used for sorting.
+    /// - Parameter comparator: A function or closure that returns a Boolean value. It should return `true` if the first element should come
+    /// before the second in the sorted array.
     mutating func customSort(_ algorithm: SortingAlgorithm, by comparator: Comparator = {$0 < $1}) {
         // Add a new case for every new sorting algorithm inside the switch statement.
         switch algorithm {
@@ -20,12 +38,33 @@ extension Array where Element: Comparable {
             bubbleSort(by: comparator)
         }
     }
+    /// Returns a sorted array using the specified algorithm.
+    ///
+    /// You do not have to specify the last parameter. By default, the array will be sorted in ascending order (as defined for the respective
+    /// type).
+    ///
+    ///     var array = [3, 1, 8, 2]
+    ///     array.customSorted(.bubbleSort)
+    ///     // [1, 2, 3, 8]
+    ///     array.customSorted(.bubbleSort, >)
+    ///     // [8, 3, 2, 1]
+    ///
+    /// If you wish to sort in descending order, pass `>` as the second parameter.
+    ///
+    /// For available algorithms, see `SortingAlgorithm`.
+    /// - Complexity: At least Ω(n), as the array has to be copied. For more details, see the documentation for the passed `algorithm` variable.
+    /// - Parameter algorithm: The algorithm to be used for sorting.
+    /// - Parameter comparator: A function or closure that returns a Boolean value. It should return `true` if the first element should come
+    /// before the second in the sorted array.
+    /// - Returns: The sorted array.
     func customSorted(_ algorithm: SortingAlgorithm, by comparator: Comparator = {$0 < $1}) -> Self {
         var copy = self
         copy.customSort(algorithm, by: comparator)
         return copy
     }
 }
+// MARK: Main Extension End
+
 
 
 // MARK: - Bubble Sort
