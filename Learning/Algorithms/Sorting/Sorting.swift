61ded3117ccbd3639570a078ad56435a2f813bbf
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-04-23 20:27:25 +0200

    Implement insertion sort

--- Learning/Algorithms/Sorting/Sorting.swift
+++ Learning/Algorithms/Sorting/Sorting.swift
@@ -9,7 +9,7 @@
 // MARK: - Main Extension
 enum SortingAlgorithm {
     /// - Complexity: O(*n*^2)
-    case bubbleSort
+    case bubbleSort, insertionSort
 }
 extension Array where Element: Comparable {
     typealias Comparator = (Element, Element) -> Bool
@@ -36,6 +36,8 @@ extension Array where Element: Comparable {
         switch algorithm {
         case .bubbleSort:
             bubbleSort(by: comparator)
+        case .insertionSort:
+            insertionSort(by: comparator)
         }
     }
     /// Returns a sorted array using the specified algorithm.
@@ -52,7 +54,7 @@ extension Array where Element: Comparable {
     /// If you wish to sort in descending order, pass `>` as the second parameter.
     ///
     /// For available algorithms, see `SortingAlgorithm`.
-    /// - Complexity: At least Ω(n), as the array has to be copied. For more details, see the documentation for the passed `algorithm` variable.
+    /// - Complexity: At least Ω(*n*), as the array has to be copied. For more details, see the documentation for the passed `algorithm` variable.
     /// - Parameter algorithm: The algorithm to be used for sorting.
     /// - Parameter comparator: A function or closure that returns a Boolean value. It should return `true` if the first element should come
     /// before the second in the sorted array.
@@ -63,19 +65,35 @@ extension Array where Element: Comparable {
         return copy
     }
 }
-// MARK: Main Extension End
 
 
 
 // MARK: - Bubble Sort
 extension Array where Element: Comparable {
-    fileprivate mutating func bubbleSort(by comparator: Comparator = {$0 < $1}) {
+    fileprivate mutating func bubbleSort(by comparator: Comparator) {
         for i in 0..<count {
             for j in 1..<count - i {
                 if comparator(self[j], self[j - 1]) { // self[j - 1] > self[j]
                     swapAt(j, j - 1)
                 }
             }
+        }
+    }
+}
+
+// MARK: - Insertion Sort
+extension Array where Element: Comparable {
+    fileprivate mutating func insertionSort(by comparator: Comparator) {
+        var i = 1
+        while i < count {
+            let x = self[i]
+            var j = i - 1
+            while j >= 0 && comparator(x, self[j]) {
+                self[j + 1] = self[j]
+                j -= 1
+            }
+            self[j + 1] = x
+            i += 1
         }
     }
 }
