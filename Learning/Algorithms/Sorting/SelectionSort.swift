a3f0a24bd2a06c3d384c4aa35b934740fd443ffb
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-04-24 13:45:26 +0200

    Implement selection sort

--- Learning/Algorithms/Sorting/SelectionSort.swift
+++ Learning/Algorithms/Sorting/SelectionSort.swift
@@ -6,3 +6,16 @@
 //  Copyright © 2020 Artem Zhukov. All rights reserved.
 //
 
+struct SelectionSort: SortingAlgorithm {
+    func sort<T>(_ array: inout Array<T>, by areInIncreasingOrder: (T, T) -> Bool) {
+        for i in 0..<array.count-1 {
+            var indexOfMinimum = i
+            for j in i..<array.count {
+                if areInIncreasingOrder(array[j], array[indexOfMinimum]) {
+                    indexOfMinimum = j
+                }
+            }
+            array.swapAt(i, indexOfMinimum)
+        }
+    }
+}
