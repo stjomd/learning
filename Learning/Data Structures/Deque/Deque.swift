ee36ef725c9d683c2c493b4be6997708f90cc85d
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-04-26 17:51:41 +0200

    Implement Deque

--- Learning/Data Structures/Deque/Deque.swift
+++ Learning/Data Structures/Deque/Deque.swift
@@ -8,6 +8,48 @@
 
 struct Deque<T> {
     
+    enum DequeSide {
+        case front, back
+    }
     
+    private var deque: DoublyLinkedList<T>
+    
+    var count: Int {
+        deque.count
+    }
+    
+    var isEmpty: Bool {
+        deque.isEmpty
+    }
+    
+    var front: T? {
+        deque.head?.value
+    }
+    
+    var back: T? {
+        deque.toe?.value
+    }
+    
+    init() {
+        deque = DoublyLinkedList<T>()
+    }
+    
+    mutating func push(_ item: T, to side: DequeSide) {
+        switch side {
+        case .front:
+            deque.insert(item, at: 0)
+        case .back:
+            deque.append(item)
+        }
+    }
+    
+    @discardableResult mutating func pop(from side: DequeSide) -> T {
+        switch side {
+        case .front:
+            return deque.removeFirst()
+        case .back:
+            return deque.removeLast()
+        }
+    }
     
 }
