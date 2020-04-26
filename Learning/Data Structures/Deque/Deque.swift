8fbce5db729180e83e75d3e4f5cee504990156e4
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-04-26 17:41:22 +0200

    Create Deque

---
+++ Learning/Data Structures/Deque/Deque.swift
@@ -0,0 +1,13 @@
+//
+//  Deque.swift
+//  Learning
+//
+//  Created by Artem Zhukov on 26.04.20.
+//  Copyright © 2020 Artem Zhukov. All rights reserved.
+//
+
+struct Deque<T> {
+    
+    
+    
+}
