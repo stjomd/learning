b59090de2128885eb081782e41293bfa94731e50
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-04-27 03:44:43 +0200

    Create CountSort.swift

---
+++ Learning/Algorithms/Sorting/CountSort.swift
@@ -0,0 +1,13 @@
+//
+//  CountSort.swift
+//  Learning
+//
+//  Created by Artem Zhukov on 27.04.20.
+//  Copyright © 2020 Artem Zhukov. All rights reserved.
+//
+
+struct CountSort: SortingAlgorithm {
+    func sort<T>(_ array: inout Array<T>, by areInIncreasingOrder: (T, T) -> Bool) {
+        <#code#>
+    }
+}
