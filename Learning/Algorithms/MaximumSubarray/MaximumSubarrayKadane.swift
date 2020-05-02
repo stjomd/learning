22b0f4ecc0b2770937c1f1b99097934333624f32
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-05-02 21:52:38 +0200

    Add Kadane's algorithm

---
+++ Learning/Algorithms/MaximumSubarray/MaximumSubarrayKadane.swift
@@ -0,0 +1,11 @@
+//
+//  MaximumSubarrayKadane.swift
+//  Learning
+//
+//  Created by Artem Zhukov on 02.05.20.
+//  Copyright © 2020 Artem Zhukov. All rights reserved.
+//
+
+extension Array where Element: AdditiveArithmetic & Comparable {
+    
+}
