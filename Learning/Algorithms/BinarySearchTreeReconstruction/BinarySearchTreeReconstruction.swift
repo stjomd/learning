43000a443ae3be7a6b023c0ebd5590920512a564
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-05-04 17:23:43 +0200

    Add BinarySearchTreeReconstruction

---
+++ Learning/Algorithms/BinarySearchTreeReconstruction/BinarySearchTreeReconstruction.swift
@@ -0,0 +1,14 @@
+//
+//  BinarySearchTreeReconstruction.swift
+//  Learning
+//
+//  Created by Artem Zhukov on 04.05.20.
+//  Copyright © 2020 Artem Zhukov. All rights reserved.
+//
+
+extension BinarySearchTree {
+    convenience init(inOrderTraversal: [T], preOrderTraversal: [T]) {
+        self.init()
+        
+    }
+}
