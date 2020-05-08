7c4af527748405f547497b6e0b5eeb2cf23daf9d
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-05-08 22:40:22 +0200

    Add Heap.swift

---
+++ Learning/Data Structures/Trees/Heap.swift
@@ -0,0 +1,15 @@
+//
+//  MaxHeap.swift
+//  Learning
+//
+//  Created by Artem Zhukov on 08.05.20.
+//  Copyright © 2020 Artem Zhukov. All rights reserved.
+//
+
+class MaxHeap<T>: BinarySearchTree<T> where T: Comparable {
+    
+}
+
+class MinHeap<T>: BinarySearchTree<T> where T: Comparable {
+    
+}
