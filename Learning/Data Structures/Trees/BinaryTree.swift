581a00ef5a660b8895e01ee0ec62eae47a716abf
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-05-08 22:49:04 +0200

    Implement BinaryTreeNode

--- Learning/Data Structures/Trees/BinaryTree.swift
+++ Learning/Data Structures/Trees/BinaryTree.swift
@@ -8,6 +8,24 @@
 
 class BinaryTreeNode<T: Comparable> {
     
+    typealias Node = BinaryTreeNode<T>
+    
+    /// The value of the node.
+    var value: T
+    /// The left child of the node.
+    var leftChild: Node?
+    /// The right child of the node.
+    var rightChild: Node?
+    /// The parent node of the node.
+    weak var parent: Node?
+    
+    /// Creates a node with the given value.
+    /// - Parameter key: The value of the node.
+    /// - Complexity: O(1)
+    init(_ value: T) {
+        self.value = value
+    }
+    
 }
 
 class BinaryTree<T: Comparable> {
