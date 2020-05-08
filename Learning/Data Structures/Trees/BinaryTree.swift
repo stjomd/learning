b9b825378f891f1b8ebfaea0a86b8477460fb90b
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-05-08 22:43:56 +0200

    Add BinaryTree.swift

---
+++ Learning/Data Structures/Trees/BinaryTree.swift
@@ -0,0 +1,15 @@
+//
+//  BinaryTree.swift
+//  Learning
+//
+//  Created by Artem Zhukov on 08.05.20.
+//  Copyright © 2020 Artem Zhukov. All rights reserved.
+//
+
+class BinaryTreeNode<T: Comparable> {
+    
+}
+
+class BinaryTree<T: Comparable> {
+    
+}
