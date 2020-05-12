1525635263ea1ead53efafdfba420ea1f2d6f529
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-05-12 21:12:17 +0200

    Add integrate(from:through:step:_:)

--- Learning/Libraries/Mathematics/Mathematics.swift
+++ Learning/Libraries/Mathematics/Mathematics.swift
@@ -7,5 +7,11 @@
 //
 
 enum Mathematics {
-    
+    static func integrate(from: Double, through: Double, step: Double = 1e-5, _ function: (Double) -> Double) -> Double {
+        var sum: Double = 0
+        for x in stride(from: from, through: through, by: step) {
+            sum += function(x + step/2) * step
+        }
+        return sum
+    }
 }
