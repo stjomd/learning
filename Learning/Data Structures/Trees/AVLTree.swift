bd6e68a6b61d81c4c2b170f610ec6c8d21c02064
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-05-12 18:45:03 +0200

    Add AVLTree.swift

---
+++ Learning/Data Structures/Trees/AVLTree.swift
@@ -0,0 +1,15 @@
+//
+//  AVLTree.swift
+//  Learning
+//
+//  Created by Artem Zhukov on 12.05.20.
+//  Copyright © 2020 Artem Zhukov. All rights reserved.
+//
+
+class AVLTreeNode<T>: BinarySearchTreeNode<T> where T: Comparable {
+    
+}
+
+class AVLTree<T>: BinarySearchTree<T> where T: Comparable {
+    
+}
