1b5a18fe13b74b1fdcfc210a8291cb08ad937869
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-05-16 23:32:23 +0200

    Implement bloom filter

--- Learning/Data Structures/BloomFilter/BloomFilter.swift
+++ Learning/Data Structures/BloomFilter/BloomFilter.swift
@@ -6,4 +6,42 @@
 //  Copyright © 2020 Artem Zhukov. All rights reserved.
 //
 
-import Foundation
+class BloomFilter<Element> {
+    
+    typealias HashFunction = (Element) -> Int
+    
+    private var mask: [Bool]
+    private var hashFunctions: [HashFunction]
+    
+    init(length: Int, hashFunctions: [HashFunction]) {
+        self.mask = Array(repeating: false, count: length)
+        self.hashFunctions = hashFunctions
+    }
+    
+    func add(_ item: Element) {
+        let positions = bits(item)
+        for i in positions {
+            mask[i] = true
+        }
+    }
+    
+    func notContains(_ item: Element) -> Bool {
+        let positions = bits(item)
+        for i in positions {
+            if !mask[i] {
+                return true
+            }
+        }
+        return false
+    }
+    
+    private func bits(_ item: Element) -> [Int] {
+        var positions = [Int]()
+        for function in hashFunctions {
+            let bit = function(item) % mask.count
+            positions.append(bit)
+        }
+        return positions
+    }
+    
+}
