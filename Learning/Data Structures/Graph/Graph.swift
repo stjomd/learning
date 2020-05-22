46a5992c61b3a8e15271b8b883002678ac3e7001
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-05-22 04:00:49 +0200

    Fix addition, implement removal of edges

--- Learning/Data Structures/Graph/Graph.swift
+++ Learning/Data Structures/Graph/Graph.swift
@@ -22,27 +22,64 @@ class Graph<Element: Hashable> {
     // aL[i] returns a list of vertices that vertex i is adjacent to
     var adjacencyList: [Element: [Element: Double?]] = [:]
     
-    private(set) var count = 0
+    private(set) var vertexCount = 0
+    private(set) var edgeCount = 0
     
     var isEmpty: Bool {
-        return count == 0
+        return vertexCount == 0
     }
     
-    func add(_ item: Element, adjacentWith: [Element]) {
-        adjacencyList[item] = [:]
+    func add(_ item: Element, adjacentWith: Element...) {
+        add(item, adjacentWith: adjacentWith)
+    }
+    func add(_ item: Element, adjacentWith: [Element] = []) {
+        add(item)
         for vertex in adjacentWith {
-            adjacencyList[item]![vertex] = 1
-            if adjacencyList[vertex] == nil {
-                adjacencyList[vertex] = [:]
-                count += 1
+            if adjacencyList[item]![vertex] == nil {
+                adjacencyList[item]![vertex] = 1
+                if adjacencyList[vertex] == nil {
+                    adjacencyList[vertex] = [:]
+                    vertexCount += 1
+                }
+                adjacencyList[vertex]![item] = 1
+                edgeCount += 1
             }
-            adjacencyList[vertex]![item] = 1
         }
-        count += 1
     }
     func add(_ item: Element) {
-        adjacencyList[item] = [:]
-        count += 1
+        if adjacencyList[item] == nil {
+            adjacencyList[item] = [:]
+            vertexCount += 1
+        }
     }
     
+    func remove(edge vertices: (Element, Element)) {
+        precondition(adjacencyList[vertices.0] != nil, "The vertex is not present in the graph")
+        precondition(adjacencyList[vertices.1] != nil, "The vertex is not present in the graph")
+        adjacencyList[vertices.0]![vertices.1] = nil
+        adjacencyList[vertices.1]![vertices.0] = nil
+    }
+    
+    func hasEdge(_ from: Element, _ to: Element) -> Bool {
+        precondition(adjacencyList[from] != nil, "The vertex is not present in the graph")
+        precondition(adjacencyList[to] != nil, "The vertex is not present in the graph")
+        return adjacencyList[from]![to] != nil && adjacencyList[to]![from] != nil
+    }
+    
+}
+
+extension Graph: CustomStringConvertible where Element: CustomStringConvertible {
+    var description: String {
+        var str = "<<Undirected, unweighted graph"
+        for v in adjacencyList {
+            var vstr = "\n\t\(v.key.description) → ["
+            for u in adjacencyList[v.key]! {
+                vstr.append("\(u.key.description), ")
+            }
+            let offset = (adjacencyList[v.key]!.count == 0) ? 0 : -2
+            let endIndex = vstr.index(vstr.endIndex, offsetBy: offset)
+            str.append(contentsOf: vstr[vstr.startIndex..<endIndex] + "]")
+        }
+        return str + "\n>>"
+    }
 }
