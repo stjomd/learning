14178c97d3ecc8bf0460f8216d1c929f60e4d4fc
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-05-22 02:01:52 +0200

    Add Graph.swift

---
+++ Learning/Data Structures/Graph/Graph.swift
@@ -0,0 +1,13 @@
+//
+//  Graph.swift
+//  Learning
+//
+//  Created by Artem Zhukov on 22.05.20.
+//  Copyright © 2020 Artem Zhukov. All rights reserved.
+//
+
+class Graph<Element> {
+    
+    
+    
+}
