99c44b1c818da73b6f0b2ef498de968947b6cb0a
Artem Zhukov <e11843614@student.tuwien.ac.at>
2020-05-23 17:21:41 +0200

    Add documentation

--- Learning/Algorithms/Graphs/Dijkstra.swift
+++ Learning/Algorithms/Graphs/Dijkstra.swift
@@ -10,16 +10,28 @@ extension DirectedGraph {
     
     // MARK: DijkstraSolution
     
+    /// A type that encapsulates data associated with the Dijkstra's algorithm.
+    ///
+    /// You can access the vertex that was used as a start vertex, as well as the distance from that vertex to any vertex in the graph. You can also calculate the shortest path from the start vertex to any vertex in the graph.
+    /// - Complexity: Required memory is O(*n*), where *n* is the number of vertices in the graph.
     struct DijkstraSolution {
         
+        /// The vertex that was used a start vertex.
         private(set) var startVertex: Element
-        private var distances: [Element: Double]
+        private var distances: [Element: Weight]
         private var predecessors: [Element: Element?]
         
-        fileprivate init(startVertex: Element, distances: [Element: Double], predecessors: [Element: Element?]) {
+        fileprivate init(startVertex: Element, distances: [Element: Weight], predecessors: [Element: Element?]) {
             self.startVertex = startVertex; self.distances = distances; self.predecessors = predecessors
         }
         
+        /// The shortest path from the start vertex to the given vertex.
+        ///
+        /// This method reconstructs the shortest path from the data that Dijkstra's algorithm provides.
+        /// - Precondition: The graph contains `vertex`.
+        /// - Complexity: O(*m*), where *m* is the number of edges in the graph.
+        /// - Parameter vertex: The target vertex.
+        /// - Returns: An array of vertices that describe a path from the start vertex to `vertex`.
         func path(to vertex: Element) -> [Element] {
             precondition(distances[vertex] != nil, "The vertex is not present in the graph")
             var path = [Element]()
@@ -33,7 +45,14 @@ extension DirectedGraph {
             return path.reversed()
         }
         
-        func distance(to vertex: Element) -> Double {
+        /// The shortest distance from the start vertex to the given vertex.
+        ///
+        /// This method returns the shortest distance that the Dijkstra's algorithm has calculated.
+        /// - Precondition: The graph contains `vertex`.
+        /// - Complexity: O(1)
+        /// - Parameter vertex: The target vertex.
+        /// - Returns: The shortest distance.
+        func distance(to vertex: Element) -> Weight {
             precondition(distances[vertex] != nil, "The vertex is not present in the graph")
             return distances[vertex]!
         }
@@ -42,13 +61,20 @@ extension DirectedGraph {
     
     // MARK: - Methods
         
+    /// Perform the Dijkstra's algorithm on the graph.
+    ///
+    /// Note that any changes to the graph might invalidate the solution. Call this method if you need the shortest distance or path to several vertices. If you only need a path to a vertex once, call `shortestPath(from:to:)` instead.
+    /// - Precondition: All edges in the graph have positive weights. The method does not check if this is the case.
+    /// - Complexity: O(*n* + *m* log *n*), where *n* is the number of vertices, and *m* is the number of edges in the graph. The returned object requires O(*n*) space.
+    /// - Parameter startVertex: The vertex from which the shortest distances are calculated.
+    /// - Returns: A `DijkstraSolution` object, which can be used to calculate the shortest paths or access the shortest distances.
     func dijkstra(from startVertex: Element) -> DijkstraSolution {
-        var distance: [Element: Double] = [startVertex: 0]
+        var distance: [Element: Weight] = [startVertex: 0]
         var predecessor = [Element: Element?]()
-        let queue = PriorityQueue<Element, Double>(<)
+        let queue = PriorityQueue<Element, Weight>(<)
         for v in vertices {
             if v != startVertex {
-                distance[v] = Double.infinity
+                distance[v] = Weight.infinity
                 predecessor[v] = nil
             }
             queue.enqueue(v, priority: distance[v]!)
@@ -68,15 +94,23 @@ extension DirectedGraph {
         return solution
     }
     
+    /// Calculates the shortest path between two given vertices.
+    ///
+    /// If you only need one path or your graph is expected to be mutated before you need another path, calling this method is more efficient as it stops as soon as the target vertex is reached.
+    /// - Precondition: All edges in the graph have positive weights. The method does not check if this is the case.
+    /// - Complexity: O(*n* + *m* log *n*), where *n* is the number of vertices, and *m* is the number of edges in the graph.
+    /// - Parameter startVertex: The vertex from which the shortest path is calculated.
+    /// - Parameter endVertex: The target vertex, the path to which is to be calculated.
+    /// - Returns: An array of vertices that describe a path from `startVertex` to `endVertex`.
     func shortestPath(from startVertex: Element, to endVertex: Element) -> [Element] {
         
-        var distance: [Element: Double] = [startVertex: 0]
+        var distance: [Element: Weight] = [startVertex: 0]
         var predecessor = [Element: Element?]()
         
-        let queue = PriorityQueue<Element, Double>(<)
+        let queue = PriorityQueue<Element, Weight>(<)
         for v in vertices {
             if v != startVertex {
-                distance[v] = Double.infinity
+                distance[v] = Weight.infinity
                 predecessor[v] = nil
             }
             queue.enqueue(v, priority: distance[v]!)
